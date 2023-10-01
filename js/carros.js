d3a31199b4298e4da7f2737952ce2c28fbe046bd
Mariana Moraes <mariana@siasp.com.br>
2023-10-01 12:47:26 -0300

    Mudando visual da seção do formulário inteligente

---
+++ js/carros.js
@@ -0,0 +1,32 @@
+const carros = [
+    {
+        nome: "Mercedes Benz Sprinter",
+        categoria: "cidade",
+        tipo: "van",
+        imagem: "https://revistacarro.com.br/wp-content/uploads/2020/04/MB-Sprinter_1.jpg"
+    },
+    {
+        nome: "Jeep Wrangler",
+        categoria: "praia",
+        tipo: "carro",
+        imagem: "https://cdn.motor1.com/images/mgl/jl2qoo/s3/2024-jeep-wrangler-front-view.webp"
+    },
+    {
+        nome: "Renault Sandero Stepway",
+        categoria: "cachoeira",
+        tipo: "carro",
+        imagem: "https://quatrorodas.abril.com.br/wp-content/uploads/2022/10/stepway10_001-1-e1665169601442.jpg?quality=70&strip=info&w=1280&h=720&crop=1"
+    },
+    {
+        nome: "Renault Kwid",
+        categoria: "cidade",
+        tipo: "carro",
+        imagem: "https://renault.grupoamazonas.com.br/wp-content/uploads/2021/11/img-renault-kwid.jpg"
+    },
+    {
+        nome: "volkswagen crossfox",
+        categoria: "fazenda",
+        tipo: "carro",
+        imagem: "https://quatrorodas.abril.com.br/wp-content/uploads/2017/12/crossfox5.jpg?quality=70&strip=info"
+    }
+]
